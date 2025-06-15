81ebcea5ea87e0953030553bbce0a5f1e6d23506
Notkirito18 <djallel123451@gmail.com>
2025-06-15 04:22:55 +0200

    working blog posts

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+// .eleventy.js
+
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  // 1. Copy over your asset folders exactly as‑is:
+  eleventyConfig.addPassthroughCopy({ "src/css": "css" });
+  eleventyConfig.addPassthroughCopy({ "src/js": "js" });
+  eleventyConfig.addPassthroughCopy({ "src/images": "images" });
+  eleventyConfig.addPassthroughCopy({ "src/admin": "admin" });
+
+  eleventyConfig.addFilter("date", (value, format = "yyyy LLL dd") => {
+    return DateTime.fromJSDate(new Date(value)).toFormat(format);
+  });
+
+  eleventyConfig.addCollection("posts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("./src/posts/*.md");
+  });
+
+  // 2. Tell Eleventy where to find source and where to write output:
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    markdownTemplateEngine: "njk", // Optional but recommended
+  };
+};
---
+++ src/posts/my-first-post.md
@@ -0,0 +1,9 @@
+---
+title: "My First Blog Post"
+date: 2024-06-01
+layout: layouts/blog-post.njk
+---
+
+Welcome to my first blog post!
+
+This is just a test post using **Markdown**.
